731e7998c2d2f5a09db68562cf3528d79e2454e9
Emmanuel <emmakyeyune@gmail.com>
2026-01-05 21:05:31 +0300

    chore: schema update (#52)

--- packages/lib/src/types/githubActionsWorkflow.ts
+++ packages/lib/src/types/githubActionsWorkflow.ts
@@ -494,6 +494,18 @@ export type ExpressionSyntax = string
 export type JobNeeds = [Name, ...Name[]] | Name
 export type Name = string
 /**
+ * You can use `jobs.<job_id>.snapshot` to generate a custom image.
+ * Add the snapshot keyword to the job, using either the string syntax or mapping syntax as shown in https://docs.github.com/en/actions/how-tos/manage-runners/larger-runners/use-custom-images#generating-a-custom-image.
+ * Each job that includes the snapshot keyword creates a separate image. To generate only one image or image version, include all workflow steps in a single job. Each successful run of a job that includes the snapshot keyword creates a new version of that image.
+ * For more information, see https://docs.github.com/en/actions/how-tos/manage-runners/larger-runners/use-custom-images.
+ */
+export type Snapshot =
+  | string
+  | {
+      'image-name': string
+      version?: string
+    }
+/**
  * You can modify the default permissions granted to the GITHUB_TOKEN, adding or removing access as required, so that you only allow the minimum required access.
  */
 export type Permissions = ('read-all' | 'write-all') | PermissionsEvent
@@ -958,6 +970,7 @@ export interface NormalJob {
    */
   name?: string
   needs?: JobNeeds
+  snapshot?: Snapshot
   permissions?: Permissions
   /**
    * The type of machine to run the job on. The machine can be either a GitHub-hosted runner, or a self-hosted runner.

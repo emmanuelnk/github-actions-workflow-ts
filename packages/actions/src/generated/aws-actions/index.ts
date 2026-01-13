9cd7642ed35e70a68d61520fd35ad5e9ea3dba0b
Ben Heidemann <ben@heidemann.dev>
2026-01-13 10:48:12 +0000

    feat: add aws-actions/configure-aws-credentials@v5

--- packages/actions/src/generated/aws-actions/index.ts
+++ packages/actions/src/generated/aws-actions/index.ts
@@ -1,6 +1,7 @@
 // This file is auto-generated. Do not edit manually.
 export * from './configure-aws-credentials/v3.js'
 export * from './configure-aws-credentials/v4.js'
+export * from './configure-aws-credentials/v5.js'
 export * from './amazon-ecr-login/v2.js'
 export * from './amazon-ecs-deploy-task-definition/v2.js'
 export * from './amazon-ecs-render-task-definition/v1.js'

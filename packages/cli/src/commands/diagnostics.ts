71943ffe71aafa439639d73af7de4201fb07585c
Ben Heidemann <ben@heidemann.dev>
2026-02-10 16:33:31 +0000

    feat(cli): output diagnostics with color

--- packages/cli/src/commands/diagnostics.ts
+++ packages/cli/src/commands/diagnostics.ts
@@ -1,9 +1,35 @@
 /* eslint-disable no-console */
 import { Diagnostics, Context } from '@github-actions-workflow-ts/lib'
 
+export const DiagnosticSeverityAnsi: Record<
+  Diagnostics.DiagnosticSeverity,
+  string
+> = {
+  trace: '\x1b[90m', // bright black / gray
+  debug: '\x1b[36m', // cyan
+  info: '\x1b[32m', // green
+  warning: '\x1b[33m', // yellow
+  error: '\x1b[31m', // red
+  fatal: '\x1b[35m', // magenta
+} as const
+
+export const ANSI_RESET = '\x1b[0m'
+
+type ConsoleDiagnosticsReporterOptions = {
+  /**
+   * Enables or disables color. When set to "auto", diagnostic logs will be
+   * colored only when running in an interactive terminal.
+   *
+   * @default "auto"
+   */
+  color?: boolean | 'auto'
+}
+
 export class ConsoleDiagnosticsReporter
   implements Diagnostics.DiagnosticsReporter
 {
+  constructor(private options: ConsoleDiagnosticsReporterOptions = {}) {}
+
   emit(d: Diagnostics.Diagnostic): void {
     // Get diagnostic rules from context
     const rules = Context.getGlobalWacContext()?.diagnosticRules
@@ -17,7 +43,7 @@ export class ConsoleDiagnosticsReporter
     }
 
     // Use the effective severity (may have been upgraded/downgraded)
-    const severity = effectiveSeverity
+    const severity = this.formatSeverity(effectiveSeverity)
 
     let message = `[github-actions-workflow-ts] ${severity}: ${d.message} (${d.code})`
     if (d.stack) {
@@ -27,5 +53,29 @@ export class ConsoleDiagnosticsReporter
       message += `\n[cause]: ${d.cause}`
     }
     console.error(message)
+  }
+
+  private formatSeverity(severity: Diagnostics.DiagnosticSeverity) {
+    if (!this.shouldUseColor()) {
+      return severity
+    }
+
+    const color = DiagnosticSeverityAnsi[severity]
+
+    if (typeof color !== 'string') {
+      return severity
+    }
+
+    return `${color}${severity}${ANSI_RESET}`
+  }
+
+  private shouldUseColor() {
+    const color = this.options.color ?? 'auto'
+
+    if (color === 'auto') {
+      return Boolean(process.stdout.isTTY)
+    }
+
+    return color
   }
 }

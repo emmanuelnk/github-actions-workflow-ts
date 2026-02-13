9567bd4f17d714f6aeb4367956cbc5889b131405
Ben Heidemann <ben@heidemann.dev>
2026-02-13 10:05:36 +0000

    ci: update draft workflow to publish a prerelease on merge to main (#105)

--- workflows/draft.wac.ts
+++ workflows/draft.wac.ts
@@ -5,6 +5,29 @@ import {
   expressions as ex,
 } from '../packages/lib/src/index.js'
 
+const betaReleaseStep = new Step({
+  name: 'Publish beta release',
+  uses: 'release-drafter/release-drafter@v5',
+  with: {
+    commitish: 'main',
+    prerelease: true,
+    'prerelease-identifier': 'beta',
+    publish: true,
+  },
+  env: {
+    GITHUB_TOKEN: ex.secret('GITHUB_TOKEN'),
+  },
+})
+
+const betaReleaseJob = new NormalJob('PublishBetaRelease', {
+  'runs-on': 'ubuntu-latest',
+  'timeout-minutes': 20,
+  if: "github.event_name == 'push'",
+  permissions: {
+    contents: 'write',
+  },
+}).addStep(betaReleaseStep)
+
 const draftStep = new Step({
   name: 'Draft next release',
   uses: 'release-drafter/release-drafter@v5',
@@ -22,7 +45,14 @@ const draftJob = new NormalJob('UpdateReleaseDraft', {
   permissions: {
     contents: 'write',
   },
-}).addStep(draftStep)
+})
+  .needs([
+    // This job must run after the beta release because otherwise
+    // release-drafter will delete the draft release.
+    // See: https://github.com/release-drafter/release-drafter/issues/1509
+    betaReleaseJob,
+  ])
+  .addStep(draftStep)
 
 export const draftWorkflow = new Workflow('draft', {
   name: 'Draft Release',
@@ -37,4 +67,4 @@ export const draftWorkflow = new Workflow('draft', {
   permissions: {
     contents: 'read',
   },
-}).addJob(draftJob)
+}).addJobs([betaReleaseJob, draftJob])
